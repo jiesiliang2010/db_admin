eec90d3f2014a5f3d1a8fd2f9085b24290b75c85
jiesiliang2010 <jiesiliang2010@163.com>
2020-04-06 10:03:46 +0800

    去除vendor目录

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('user-api')->group(function () {
+    Route::get('/user', function () {echo 111111;});
+});
