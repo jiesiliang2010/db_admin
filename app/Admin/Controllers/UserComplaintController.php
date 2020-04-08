ac86d86521443f227a811188e88be36be5d62bc8
cgb62867258 <cgb62867258@sina.com>
2020-04-08 17:37:34 +0800

    增加举报功能

---
+++ app/Admin/Controllers/UserComplaintController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+//举报申诉模块功能
+//author:陈国宾
+//2020.4.8
+class UserComplaintController extends Controller
+{
+    //搜索过滤
+    private function filter($query,$where){
+        if($where['complaint_process_result']){
+            $query->where("e.complaint_process_result",$where['complaint_process_result']);
+        }
+
+        if($where['is_myself']){
+            $query->where("e.complaint_process_operator_id",Auth::guard("admin")->id());
+        }
+
+        if($where['report_no']){
+            $query->where("a.report_no",$where['report_no']);
+        }
+
+        if($where['report_date_s']){
+            $query->where("a.report_process_date",">=",strtotime($where['report_date_s']));
+        }
+
+        if($where['report_date_e']){
+            $query->where("a.report_process_date","<=",strtotime($where['report_date_e']));
+        }
+
+        if($where['complaint_date_s']){
+            $query->where("e.complaint_date",">=",strtotime($where['complaint_date_s']));
+        }
+
+        if($where['complaint_date_e']){
+            $query->where("e.complaint_date","<=",strtotime($where['complaint_date_e']));
+        }
+
+        if($where['type']){
+            $query->where("a.type",$where['type']);
+        }
+
+        if($where['user_name']){
+            $query->where("b.nickname",$where['user_name']);
+        }
+
+        if($where['sale_name']){
+            $query->where("c.nickname",$where['sale_name']);
+        }
+
+        if($where['report_type']){
+            $query->where("a.report_type",$where['report_type']);
+        }
+
+        return $query;
+    }
+
+    //申诉列表
+    public function list(Request $request){
+        $where['complaint_process_result'] = $request->input("complaint_process_result");
+        $where['is_myself'] = $request->input("is_myself");
+        $where['report_no'] = $request->input("report_no");
+        $where['report_date_s'] = $request->input("report_date_s");
+        $where['report_date_e'] = $request->input("report_date_e");
+        $where['type'] = $request->input("type");
+        $where['user_name'] = $request->input("user_name");
+        $where['sale_name'] = $request->input("sale_name");
+        $where['report_type'] = $request->input("report_type");
+        $where['complaint_date_s'] = $request->input("complaint_date_s");
+        $where['complaint_date_e'] = $request->input("complaint_date_e");
+
+        $query = DB::table("report_complaint as e");
+
+        $query = $this->filter($query,$where);
+
+        $res = $query->leftJoin("user_report as a","e.report_id","=","a.id")
+            ->leftJoin("users as b","a.user_id","=","b.id")
+            ->leftJoin("salesman as c","a.sale_id","=","c.id")
+            ->select("e.*","a.*","b.nickname as user_name","c.nickname as sale_name")
+            ->paginate(15);
+
+        return $this->ok($res);
+    }
+
+    //申诉详情
+    public function detail(Request $request){
+        $where['report_no'] = $request->input("report_no");
+
+        $query = DB::table("report_complaint as e");
+
+        $query = $this->filter($query,$where);
+
+        $res = $query->leftJoin("user_report as a","e.report_id","=","a.id")
+            ->leftJoin("users as b","a.user_id","=","b.id")
+            ->leftJoin("salesman as c","a.sale_id","=","c.id")
+            ->select("e.*","a.*","b.nickname as user_name","c.nickname as sale_name")
+            ->first();
+
+        return $this->ok($res);
+    }
+
+    //处理申诉
+    public function update(Request $request){
+        $where['report_id'] = $request->input("report_id");
+        $save['complaint_process_date'] = time();
+        $save['complaint_process_operator_id'] = Auth::guard("admin")->id();
+        $save['complaint_process_result'] = $request->input("complaint_process_result");
+        $save['complaint_process_remark'] = $request->input("complaint_process_remark");
+
+        if(!$where['report_id']){
+            return $this->error("没有这个申诉单");
+        }
+
+        if($save['report_process_result'] <=0){
+            return $this->error("请选择正确的处理方式");
+        }
+
+        $res = DB::table("report_complaint")
+            ->where($where)
+            ->save($save);
+
+        return $this->ok($res);
+    }
+}
---
+++ app/Admin/Controllers/UserReportController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+//举报模块功能
+//author:陈国宾
+//2020.4.7
+class UserReportController extends Controller
+{
+    //搜索过滤
+    private function filter($query,$where){
+        if($where['report_process_result']){
+            $query->where("a.report_process_result",$where['report_process_result']);
+        }
+
+        if($where['is_myself']){
+            $query->where("a.report_process_operator_id",Auth::guard("admin")->id());
+        }
+
+        if($where['report_no']){
+            $query->where("a.report_no",$where['report_no']);
+        }
+
+        if($where['report_date_s']){
+            $query->where("a.report_process_date",">=",strtotime($where['report_date_s']));
+        }
+
+        if($where['report_date_e']){
+            $query->where("a.report_process_date","<=",strtotime($where['report_date_e']));
+        }
+
+        if($where['type']){
+            $query->where("a.type",$where['type']);
+        }
+
+        if($where['user_name']){
+            $query->where("b.nickname",$where['user_name']);
+        }
+
+        if($where['sale_name']){
+            $query->where("c.nickname",$where['sale_name']);
+        }
+
+        if($where['report_type']){
+            $query->where("a.report_type",$where['report_type']);
+        }
+
+        return $query;
+    }
+
+    //举报列表
+    public function list(Request $request){
+        $where['report_process_result'] = $request->input("report_process_result");
+        $where['is_myself'] = $request->input("is_myself");
+        $where['report_no'] = $request->input("report_no");
+        $where['report_date_s'] = $request->input("report_date_s");
+        $where['report_date_e'] = $request->input("report_date_e");
+        $where['type'] = $request->input("type");
+        $where['user_name'] = $request->input("user_name");
+        $where['sale_name'] = $request->input("sale_name");
+        $where['report_type'] = $request->input("report_type");
+
+        $query = DB::table("user_report as a");
+
+        $query = $this->filter($query,$where);
+
+        $res = $query->leftJoin("users as b","a.user_id","=","b.id")
+        ->leftJoin("salesman as c","a.sale_id","=","c.id")
+        ->select("a.*","b.nickname as user_name","c.nickname as sale_name")
+        ->paginate(15);
+
+        return $this->ok($res);
+    }
+
+    //举报详情
+    public function detail(Request $request){
+        $where['report_no'] = $request->input("report_no");
+
+        $query = DB::table("user_report as a");
+
+        $query = $this->filter($query,$where);
+
+        $res = $query->leftJoin("users as b","a.user_id","=","b.id")
+        ->leftJoin("salesman as c","a.sale_id","=","c.id")
+        ->select("a.*","b.nickname as user_name","c.nickname as sale_name")
+        ->first();
+
+        return $this->ok($res);
+    }
+
+    //处理举报
+    public function update(Request $request){
+        $where['report_no'] = $request->input("report_no");
+        $save['report_process_operator_id'] = Auth::guard("admin")->id();
+        $save['report_process_result'] = $request->input("report_process_result");
+        $save['report_process_type'] = $request->input("report_process_type");
+        $save['report_process_remark'] = $request->input("report_process_remark");
+        $save['report_process_date'] = time();
+
+        if(!$where['report_no']){
+            return $this->error("没有这个举报单");
+        }
+
+        if($save['report_process_type'] <=0 || $save['report_process_result'] <=0){
+            return $this->error("请选择正确的处理方式");
+        }
+
+        $res = DB::table("user_report")
+        ->where($where)
+        ->save($save);
+
+        return $this->ok($res);
+    }
+}
